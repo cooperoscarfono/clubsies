a596e508c35fea627b72be0216ed3f3290cac788
baaash <bleat@baaa.sh>
2023-06-10 19:53:12 +1200

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-img height="100" src="@/assets/logo.svg" />
+
+      <h1 class="text-h2 font-weight-bold">Clubsies</h1>
+
+      <h2 class="text-cursive">come together</h2>
+      <div class="py-14" />
+
+      <v-row class="d-flex align-center justify-center">
+        <v-col cols="auto">
+          <v-btn
+            href="#"
+            min-width="164"
+            rel="noopener noreferrer"
+            target="_blank"
+            variant="text"
+          >
+            <v-icon icon="mdi-account-group" size="large" start />
+
+            Find a club
+          </v-btn>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-btn
+            color="primary"
+            href="#"
+            min-width="228"
+            rel="noopener noreferrer"
+            size="x-large"
+            target="_blank"
+            variant="flat"
+          >
+            <v-icon icon="mdi-check" size="large" start />
+
+            Start a club
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
+<style scoped>
+  h2 {
+    font-family: "Nothing You Could Do";
+  }
+</style>
